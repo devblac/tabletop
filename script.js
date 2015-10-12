372ce3e6a09e39ed2dec2789b3b58daa36fed000
unknown <julian.blacher@gmail.com>
2015-10-11 23:41:46 -0300

    Here we will have the algorithm to search in Mercadolibre.com

---
+++ script.js
@@ -0,0 +1,7 @@
+<script src="src/tabletop.js" />
+
+function init() {
+  Tabletop.init( { key: '13hxmBDw6slVy4Ho6ZrB7JOmASRFVOv4CWRJp0s6vZzg',
+                   callback: function(data, tabletop) { console.log(data) },
+                   simpleSheet: true } )
+}
